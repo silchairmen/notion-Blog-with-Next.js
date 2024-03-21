c1ebce4ba452facf8a597e58e6fb9e26fd7dfed3
Jalnik <silmusic0113@gmail.com>
2024-03-21 17:54:30 +0900

    [fix] sitemap fixed?

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,32 +9,23 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         {
             url: 'https://jalnik.vercel.app',
             lastModified: new Date(),
-            changeFrequency: 'yearly',
-            priority: 1,
         },
         {
             url: 'https://jalnik.vercel.app/introduce',
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.8,
         },
         {
             url: 'https://jalnik.vercel.app/lecture',
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.8,
             },
         
         {
             url: 'https://jalnik.vercel.app/docs',
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
         }, 
         ...(await posts.results).map((post: any) => ({
             url: `https://jalnik.vercel.app/post/${encodeURIComponent(post.properties.이름.title[0].plain_text)}`,
             lastModified: `${post.last_edited_time}`,
-            priority: 0.7
         }))
     ]
 }
